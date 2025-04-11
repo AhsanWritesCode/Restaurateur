8915df00df1a34f467c636eb4a8ec496820f8b21
“30119655” <ahsan.tariq@ucalgary.ca>
2025-04-10 18:49:16 -0600

    created basic landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const App = () => {
     return (
-        <div>
-            <h1>Hello World</h1>
+        <div className="landing-page">
+            <header className="header">
+                <h1>Welcome to Restauranteur</h1>
+                <p>As a valued customer, view our available menu items and book a reservation in advance.</p>
+                <p>As a valued employee, get signed in and log your hours view/edit your information, and view available inventory.</p>
+            </header>
         </div>
     );
 };
